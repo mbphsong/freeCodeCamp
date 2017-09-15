306b58611dbdcde280520e31e03d9805c78fc76e
Erin Halbmaier <katzenlieben@hotmail.com>
2017-09-14 19:56:10 -0500

    Added 7-day forecast

--- js/localWeather.js
+++ js/localWeather.js
@@ -12,20 +12,19 @@ $(document).ready(function dR() {
         var units = "fahrenheit";
         var updateWeather = setInterval(function uW() {
             if(weatherLocation == "local") {
-                getCurrentWeather();
+                getLocalWeather();
             } else {
                 doAjaxStuff(points);
             }
         }, frequency);
 
-        function updateTemp(measurement, currTemp) {
-            var dispMeas = measurement == "celsius" ? "C" : "F";
+        function convertTemp(measurement, currTemp) {
             var newTemp = measurement == "celsius" ? (currTemp - 32) / 1.8 : currTemp * 1.8 + 32;
-            $(".temp").html(Math.round(newTemp));
-            $(".measurement").html(dispMeas);
+            
+            return Math.round(newTemp);
         }
 
-        function getCurrentWeather() {
+        function getLocalWeather() {
             if (!navigator.geolocation) {
                 $(".location").html("Location services not available in your browser");
                 return;
@@ -39,19 +38,32 @@ $(document).ready(function dR() {
         }
 
         function doAjaxStuff(points) {
+            //clear any errors
+            $(".location-error-message").html("").removeClass("alert").removeClass("alert-danger");
+            //get location text
             $.ajax({
                 url: "https://api.weather.gov/points/" + points,
                 success: locSuccess,
+                error: badLocation,
                 cache: false
             });
-        
+            //get station so that can get current weather
             $.ajax({
                 url: "https://api.weather.gov/points/" + points + "/stations",
                 success: stationSuccess,
+                error: clearData,
+                cache: false
+            });
+            //get forecast
+            $.ajax({
+                url: "https://api.weather.gov/points/" + points + "/forecast",
+                success: forecastSuccess,
+                error: clearData,
                 cache: false
             });
             
             function locSuccess(data) {
+                console.log("loc success");
                 var location = data["properties"]["relativeLocation"]["properties"];
     
                 $(".location").html(location["city"] + ", " + location["state"]);
@@ -72,17 +84,52 @@ $(document).ready(function dR() {
             function obsSuccess(data) {
                 var props = data["properties"];
                 var currTemp = props["temperature"]["value"];
+                console.log(currTemp);
                 var conditions = props["textDescription"];
                 var bg = props["icon"];
+                var convertedTemp = units == "fahrenheit" ? convertTemp("fahrenheit",currTemp) : Math.round(currTemp);
                 
-                if ($("#fahrenheit").hasClass("active")) {
-                    updateTemp("fahrenheit",currTemp);
-                } else {
-                    $(".temp").html(currTemp);
-                }
-                
-                $(".conditions").html(conditions);
+                // if ($("#fahrenheit").hasClass("active")) {
+                //     convertedTemp = convertTemp("fahrenheit",currTemp);
+                //  }
+                $(".current-weather .temp").html(convertedTemp);
+                $("#current .conditions").html(conditions);
                 setWeatherIcon(bg,conditions);
+            }
+            function forecastSuccess(data) {
+                var periods = data["properties"]["periods"];
+                if (!periods[0]["isDaytime"]) {
+                    var xtra = periods[0];
+                    periods.unshift(xtra);
+                    $("#collapse-0 .forecast.first").hide();
+                } else {
+                    $("#collapse-0 .forecast.first").show();
+                }
+
+                for (var i=0; i<7; i++) {
+                    var panelInfo = {
+                        per0: periods[i * 2],
+                        per1: periods[i * 2 + 1],
+                    }
+                    var dayText = i == 0 ? "Today" : panelInfo.per0["name"];
+                    // var panel = $("#collapse-" + i);
+
+                    $("#day-" + i + " h4 a").html(dayText);
+
+                    $("#collapse-" + i + " .panel-body").each(function fillPanels(index) {
+                        var thisPanelInfo = panelInfo["per" + index];
+                        var thisPanel = $(this);
+                        var highLow = thisPanelInfo["isDaytime"] ? "High: " : "Low: ";
+                        var forecastTemp = (thisPanelInfo["temperatureUnit"] == "F" && units == "celsius") || (thisPanelInfo["temperatureUnit"] == "C" && units == "fahrenheit") ? convertTemp(units,thisPanelInfo["temperature"]) : thisPanelInfo["temperature"];
+                        
+                        thisPanel.find("h5").html(thisPanelInfo["name"]);
+                        thisPanel.find("img").attr("src",thisPanelInfo["icon"]);
+                        thisPanel.find(".hiLo").html(highLow);
+                        thisPanel.find(".temp").html(forecastTemp);
+                        thisPanel.find(".wind").html("Wind: " + thisPanelInfo["windDirection"] + " " + thisPanelInfo["windSpeed"]);
+                        thisPanel.find(".short").html(thisPanelInfo["shortForecast"]);
+                    });
+                }
             }
             
             function setWeatherIcon(bg, conditions) {
@@ -98,6 +145,7 @@ $(document).ready(function dR() {
                 switch (conditions) {
                     case "Sunny":
                     case "Partly Sunny":
+                    case "Mostly Clear":
                     case "Clear":
                         weatherIcon += "sunny";
                         break;
@@ -118,16 +166,43 @@ $(document).ready(function dR() {
             }
         }
 
+        function clearData(data) {
+            var locText = weatherLocation == "local" ? "your location" : "the specified location";
+            $(".location").html("Weather data is unavailable at this time for " + locText + ".");
+
+            $(".temp").html("--");
+            $(".current-weather i").attr("class","wi wi-na");
+            $(".conditions").html("");
+            $(".wind").html("Wind: --");
+            $(".short").html("");
+            $(".forecast img").attr("src","");
+            $("#station").html("-unavailable-");
+        }
+
+        function badLocation(data) {
+            $(".location-error-message").html("We're sorry, we are unable to find weather data for the specified location").addClass("alert").addClass("alert-danger");
+    
+        }
+
         function flipActive(group,selected) {
             $(group).find("button").removeClass("active");
             $("#" + selected).addClass("active");
 
         }
-        function toggleUnits(unitsGroup, units) {
-            if (!$("#" + units).hasClass("active")) {
+        function toggleUnits(unitsGroup, selected) {
+            if (!$("#" + selected).hasClass("active")) {
                 //make sure we didn't click the current active button
-                flipActive(unitsGroup, units);
-                updateTemp(units, $(".temp").text());
+                var dispMeas = selected == "celsius" ? "C" : "F";
+                var newTemp;
+                units = selected;
+                flipActive(unitsGroup, selected);
+                // newTemp = updateTemp(units, $(".temp").text());
+                // $(".temp").html(newTemp);
+                $(".temp").each(function uT() {
+                    newTemp = convertTemp(selected,$(this).text());
+                    $(this).html(newTemp);
+                });
+                $(".measurement").html(dispMeas);
             }
         }
 
@@ -137,6 +212,7 @@ $(document).ready(function dR() {
             $.ajax({
                 url: "https://maps.google.com/maps/api/geocode/json?address=" + targetLoc.replace(/ /g, "+"),
                 success: googSuccess,
+                error: badLocation,
                 cache: false
             });
 
@@ -158,7 +234,7 @@ $(document).ready(function dR() {
                     //don't want to see input for other location if using local
                     $(locInputField).parent().hide();
 
-                    getCurrentWeather();
+                    getLocalWeather();
                 } else {
                     $(locInputField).parent().show();
                     if ($(locInputField).val() != "") {
@@ -171,7 +247,7 @@ $(document).ready(function dR() {
         }
 
         //set up initial data
-        getCurrentWeather();
+        getLocalWeather();
         return {
             toggleUnits: toggleUnits,
             setLocation: setLocation,
